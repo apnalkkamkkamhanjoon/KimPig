eb6f40775d715dc812e30895949e09af6395d354
한준 <s22073@gsm.hs.kr>
2023-04-28 15:06:09 +0900

    👀:: 불필요한 파일 삭제

--- src/components/Know.tsx
+++ src/components/Know.tsx
@@ -14,6 +14,7 @@ type KnowProps = {
 };
 
 const Know = ({ data }: KnowProps) => {
+
   const goToTop = () => {
     // top:0 >> 맨위로  behavior:smooth >> 부드럽게 이동할수 있게 설정하는 속성
     window.scrollTo({ top: 0, behavior: "smooth" });
